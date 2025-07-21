64e92a50ed75b2f5f22c28ce6f9c65246c5adf92
Fariha127 <142403936+Fariha127@users.noreply.github.com>
2025-07-21 22:53:36 +0600

    Update

--- 35. Search Insert Position.cpp
+++ 35. Search Insert Position.cpp
@@ -1,12 +1,36 @@
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int i = 0;
+//         for( i= 0; i<nums.size(); i++) {
+//             if(target <= nums[i]){
+//                 break;
+//             }
+//         }
+//       return i;  
+//     }
+// };
+
+// using binary search :
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int i = 0;
-        for( i= 0; i<nums.size(); i++) {
-            if(target <= nums[i]){
-                break;
-            }
+      int l = 0;
+      int r = nums.size() - 1;
+      int m = 0;
+    
+
+      while (l <= r) {
+        m = ( l + r ) / 2;
+        if ( nums[m] < target) {
+      
+            l = m + 1;
         }
-      return i;  
+        else if (nums[m] >= target) {
+           
+            r = m - 1;
+        }
+        
+      }
+      return l;
     }
-};
